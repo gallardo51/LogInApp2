741a57502a0d4db87651c5a129333d5d5230b429
Alex Sobolev <31108@mail.ru>
2023-11-12 14:32:54 +0300

    Setup InfoViewController

--- LogInApp2/Model/UserModel.swift
+++ LogInApp2/Model/UserModel.swift
@@ -11,20 +11,38 @@ struct User {
     let person: Person
     
     static func getUserData() -> User {
-        User(login: "User", password: "Password", person: Person.getPerson())
+        User(
+            login: "User",
+            password: "Password",
+            person: Person.getPerson()
+        )
     }
 }
 
 struct Person {
     let name: String
     let surname: String
-    
+    let userImage: String
+    let bornDate: String
+    let zodiacSing: String
+    let cityFrom: String
+    let cellPhone: String
+    let hobby: String
     
     var fullName: String {
         "\(name) \(surname)"
     }
     
     static func getPerson() -> Person {
-        Person(name: "Alex", surname: "Sobolev")
+        Person(
+            name: "Александр",
+            surname: "Соболев",
+            userImage: "AlexPH",
+            bornDate: "08.07.1988",
+            zodiacSing: "Рак",
+            cityFrom: "Воронеж",
+            cellPhone: "89610297502",
+            hobby: "Программирование"
+        )
     }
 }
--- LogInApp2/ViewControllers/InfoViewController.swift
+++ LogInApp2/ViewControllers/InfoViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class InfoViewController: UIViewController {
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var bornDateLabel: UILabel!
+    @IBOutlet weak var zodiacSingLabel: UILabel!
+    @IBOutlet weak var cityFromLabel: UILabel!
+    @IBOutlet weak var cellPhoneLabel: UILabel!
+    @IBOutlet weak var hobbyLabel: UILabel!
     
     var user: User!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = user.person.name
+        title = user.person.fullName
+        
+        userImageView.image = UIImage(named: user.person.userImage)
+        bornDateLabel.text = "\(user.person.bornDate)"
+        zodiacSingLabel.text = "\(user.person.zodiacSing)"
+        cityFromLabel.text = "\(user.person.cityFrom)"
+        cellPhoneLabel.text = "\(user.person.cellPhone)"
+        hobbyLabel.text = "\(user.person.hobby)"
     }
 }
